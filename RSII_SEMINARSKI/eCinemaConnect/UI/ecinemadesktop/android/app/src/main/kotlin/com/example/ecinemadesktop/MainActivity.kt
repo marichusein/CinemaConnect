06f42a185dc45385a899ed5bad6a5cb45c6b263f
Husein Marić <79978304+marichusein@users.noreply.github.com>
2023-08-18 13:35:27 +0200

    Desktop UI initial

---
+++ RSII_SEMINARSKI/eCinemaConnect/UI/ecinemadesktop/android/app/src/main/kotlin/com/example/ecinemadesktop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ecinemadesktop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ RSII_SEMINARSKI/eCinemaConnect/UI/ecinemadesktop/lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LoginApp());
+}
+
+class LoginApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cinema Login App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  void _performLogin() {
+    String enteredUsername = _usernameController.text;
+    String enteredPassword = _passwordController.text;
+
+    // TODO: Implement actual authentication logic
+    // For now, let's just print the entered credentials
+    print("Entered Username: $enteredUsername");
+    print("Entered Password: $enteredPassword");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.white, // Changed color to white
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.movie,
+                    size: 80,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      prefixIcon: Icon(Icons.person, color: Colors.blue),
+                      border: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: Icon(Icons.lock, color: Colors.blue),
+                      border: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  ElevatedButton(
+                    onPressed: _performLogin,
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                    ),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/cinema1.png',
+                    height: 250,
+                  ),
+                  SizedBox(height: 40),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // TODO: Navigate to create new account screen
+                    },
+                    icon: Icon(Icons.person_add),
+                    label: Text('Create New Account'),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // TODO: Navigate to add user screen
+                    },
+                    icon: Icon(Icons.person_add_alt),
+                    label: Text('Add User'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
