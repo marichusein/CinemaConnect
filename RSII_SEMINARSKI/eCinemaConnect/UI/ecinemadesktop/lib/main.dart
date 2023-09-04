8148807226ecd8b6d0cb683697ca191d781d41fc
Husein Marić <79978304+marichusein@users.noreply.github.com>
2023-09-04 19:25:09 +0200

    Login basic

--- RSII_SEMINARSKI/eCinemaConnect/UI/ecinemadesktop/lib/main.dart
+++ RSII_SEMINARSKI/eCinemaConnect/UI/ecinemadesktop/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:ecinemadesktop/forms/movieform.dart';
+import 'package:ecinemadesktop/forms/MovieForm.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,22 +25,22 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController _usernameController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
-  void _performLogin() {
-  String enteredUsername = _usernameController.text;
-  String enteredPassword = _passwordController.text;
+  void _performLogin(BuildContext context) {
+    String enteredUsername = _usernameController.text;
+    String enteredPassword = _passwordController.text;
 
-  // TODO: Implement actual authentication logic
-  // For now, let's just print the entered credentials
-  print("Entered Username: $enteredUsername");
-  print("Entered Password: $enteredPassword");
+    // TODO: Implement actual authentication logic
+    // For now, let's just print the entered credentials
+    print("Entered Username: $enteredUsername");
+    print("Entered Password: $enteredPassword");
 
-  // Navigate to the MovieForm screen after login
-  Navigator.of(context).push(
-    MaterialPageRoute(
-      builder: (context) => MovieForm(), // Create an instance of MovieForm
-    ),
-  );
-}
+    // Navigate to the UserDashboard screen after login
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => UserDashboard(), // Create an instance of UserDashboard
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +50,7 @@ class _LoginPageState extends State<LoginPage> {
           Expanded(
             flex: 1,
             child: Container(
-              color: Colors.white, // Changed color to white
+              color: Colors.white,
               padding: const EdgeInsets.all(20.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -87,7 +87,7 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   SizedBox(height: 40),
                   ElevatedButton(
-                    onPressed: _performLogin,
+                    onPressed: () => _performLogin(context), // Pass context to _performLogin
                     style: ElevatedButton.styleFrom(
                       primary: Colors.blue,
                       padding: EdgeInsets.symmetric(horizontal: 40),
@@ -115,7 +115,7 @@ class _LoginPageState extends State<LoginPage> {
                   SizedBox(height: 40),
                   ElevatedButton.icon(
                     onPressed: () {
-                      // TODO: Navigate to create new account screen
+                      // TODO: Navigate to create a new account screen
                     },
                     icon: Icon(Icons.person_add),
                     label: Text('Create New Account'),
@@ -134,6 +134,110 @@ class _LoginPageState extends State<LoginPage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class UserDashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Dashboard'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              // Navigate back to the LoginPage
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => LoginPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.person,
+                      size: 60,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'User Name',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      _buildNavItem(Icons.movie, 'Add Movie', context, MovieForm()), // Pass MovieForm as the destination
+                      _buildNavItem(Icons.person, 'Add Actor', context, MovieForm()/* Add Actor screen here */), // Add Actor destination
+                      // Add more navigation items as needed
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.white,
+              child: const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'CINEMACONNECT',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    // Add your content for the dashboard here
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, String label, BuildContext context, Widget destination) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.white),
+      title: Text(
+        label,
+        style: TextStyle(color: Colors.white),
+      ),
+      onTap: () {
+        // Navigate to the specified destination widget
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => destination,
+          ),
+        );
+      },
     );
   }
 }
